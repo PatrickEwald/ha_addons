ff18ae2994319b55186551a9767813da5f514acd
PatrickEwald <p@trick-ewald.de>
2024-10-20 16:05:31 +0200

    server

--- imageConverter/run.sh
+++ imageConverter/run.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
-# Pfade für Eingangs- und Ausgangsbild
-INPUT_IMAGE="/media/input.jpg"
-OUTPUT_IMAGE="/media/output.webp"
+echo "Hello world!"
 
-# FFmpeg-Befehl zur Konvertierung
-ffmpeg -i "$INPUT_IMAGE" -c:v libwebp -q:v 80 "$OUTPUT_IMAGE"
-
-echo "Bild wurde erfolgreich von JPEG zu WebP konvertiert."
+python3 -m http.server 8000
